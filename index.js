8ed0505b45bc84e0e1525c3bc5bd06c8f717a6dd
MD.AL-AMIN <alaminhossainmomenul87@gmail.com>
2025-06-17 20:20:54 +0600

    done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://food-sharing.netlify.app"],
     credentials: true,
   })
 );
@@ -80,7 +80,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // -----user db
     const userCollection = client
       .db("foodSharing")
@@ -239,10 +239,10 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
